373592dc5f44b96e7ba796b085c51ac3d6a0cab8
Ana Mafalda Batista <95191872+AnaMafaldaBatista@users.noreply.github.com>
2022-02-23 16:01:49 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,6 @@ return `${day} ${hours}:${minutes}`;
 
 
 function displayTemperature(response) {
-console.log(response.data);    
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
@@ -28,11 +27,13 @@ temperatureElement.innerHTML = Math.round (response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round (response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "6a3bbc390ca7b91e7be8573895ce508c"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
